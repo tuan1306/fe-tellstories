708b71affd7e75eb0315689a88e60c90fe46fa2f
antuan1306 <tuantase172217@fpt.edu.vn>
2025-08-06 23:01:12 +0700

    Changed the import for Home page

--- src/app/(auth)/page.tsx
+++ src/app/(auth)/page.tsx
@@ -1,14 +1,5 @@
-import HelloWorld from "@/components/misc/HelloWorld";
-import { Button } from "@/components/ui/button";
-import Link from "next/link";
+import Login from "./login/page";
 
 export default function Home() {
-  return (
-    <div className="h-screen flex items-center justify-center flex-col gap-8">
-      <HelloWorld />
-      <Button className="bg-slate-500 blur-sm hover:blur-none cursor-pointer">
-        <Link href="/login"> To the another side of the world!</Link>
-      </Button>
-    </div>
-  );
+  return <Login />;
 }
